fdacf4989192108f4c5596dd99ce8dfa29637a6b
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2026-04-05 07:47:19 +0900

    format

--- src/pages/[redirects].ts
+++ src/pages/[redirects].ts
@@ -1,26 +1,22 @@
-import { getOrganizations } from '../lib/organizations';
+import { getOrganizations } from "../lib/organizations";
 
 const redirects: Record<string, string> = {};
 const orgs = await getOrganizations();
 
 for (const org of orgs) {
-    if (org.old_id) {
-        redirects[ `/orgs/${org.old_id}` ] = `/orgs/${org.id}`;
-    }
+  if (org.old_id) {
+    redirects[`/orgs/${org.old_id}`] = `/orgs/${org.id}`;
+  }
 }
 
 const fileContent = Object.entries(redirects)
-    .map(([oldPath, newPath]) => `${oldPath} ${newPath} 301`)
-    .join("\n");
+  .map(([oldPath, newPath]) => `${oldPath} ${newPath} 301`)
+  .join("\n");
 
 export async function getStaticPaths() {
-
-    return [
-        { params: { redirects: "_redirects" } },
-    ];
+  return [{ params: { redirects: "_redirects" } }];
 }
 
 export async function GET() {
-    return new Response(fileContent);
+  return new Response(fileContent);
 }
-
